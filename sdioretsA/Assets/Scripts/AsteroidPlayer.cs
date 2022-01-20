86031b07612dbb41f67c7b78bce113a0a4f3e10e
Voidraizer <splutikoff@gmail.com>
2022-01-19 22:53:50 -0500

    Skeleton of AsteroidPlayer
    * Made it so you left click and spawns an asteroid
    * If you hold the mouse button, the asteroid will continue to grow in size and shift sprites to higher res ones as it grows up to a maximum size
    * Also limited the minimum size an asteroid can be created so you can create untargetable space dust by rapidly clicking.  The growing asteroid will always reach a certain phase before being let go

--- sdioretsA/Assets/Scripts/AsteroidPlayer.cs
+++ sdioretsA/Assets/Scripts/AsteroidPlayer.cs
@@ -14,39 +14,110 @@ public class AsteroidPlayer : MonoBehaviour
     private GameObject asteroid1Lvl3Prefab;
 
     [SerializeField]
-    private float growthRate;
-    [SerializeField]
-    private float maxSize;
-
-    [SerializeField]
     private Camera mainCamera;
 
+    [SerializeField]
+    private float growthRateDefault;
+    [SerializeField]
+    private float growthRateDecay;
+
+    [SerializeField]
+    private Vector3 startingAsteroidScale;
+
     private bool haveAsteroid = false;
+    private bool doneGrowing = true;
+
+    private int asteroidLevel;
+
+    private float growthRateActual;
+
+    private GameObject heldAsteroid;
+
+    private Vector3 asteroidScale;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        asteroidScale = startingAsteroidScale;
+        growthRateActual = growthRateDefault;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 mouseLocation = mainCamera.ScreenToWorldPoint( Input.mousePosition );
+        mouseLocation.z = -0.04f;
+
         if( Input.GetButton( "Fire1" ) )
         {
-            Physics.Raycast( mainCamera.transform.position, Input.mousePosition, 10f );
-
-            if( !haveAsteroid )
+            if( !haveAsteroid && doneGrowing )
             {
+                heldAsteroid = Instantiate( asteroidLvl0Prefab, mouseLocation, Quaternion.identity );
+                heldAsteroid.transform.localScale = asteroidScale;
+                asteroidLevel = 0;
+                haveAsteroid = true;
+                doneGrowing = false;
+            }
+        }
+        else if( Input.GetButtonUp( "Fire1" ) )
+        {
+            haveAsteroid = false;
+        }
 
+        if( haveAsteroid || !doneGrowing )
+        {
+            heldAsteroid.transform.position = mouseLocation;
+            asteroidScale = new Vector3( asteroidScale.x + ( growthRateActual * Time.deltaTime ), asteroidScale.y + ( growthRateActual * Time.deltaTime ), 1f );
+
+            if( ( heldAsteroid.transform.localScale.x >= 1f ) && ( heldAsteroid.transform.localScale.y >= 1f ) )
+            {
+                switch( asteroidLevel )
+                {
+                    case 0:
+                        Destroy( heldAsteroid );
+                        heldAsteroid = Instantiate( asteroid1Lvl1Prefab, mouseLocation, Quaternion.identity );
+                        asteroidScale = new Vector3( 0.5f, 0.5f, 1f );
+                        asteroidLevel++;
+                        growthRateActual *= growthRateDecay;
+                        break;
+                    case 1:
+                        Destroy( heldAsteroid );
+                        heldAsteroid = Instantiate( asteroid1Lvl2Prefab, mouseLocation, Quaternion.identity );
+                        asteroidScale = new Vector3( 0.5f, 0.5f, 1f );
+                        asteroidLevel++;
+                        growthRateActual *= growthRateDecay;
+                        doneGrowing = true;
+                        break;
+                    case 2:
+                        Destroy( heldAsteroid );
+                        heldAsteroid = Instantiate( asteroid1Lvl3Prefab, mouseLocation, Quaternion.identity );
+                        asteroidScale = new Vector3( 0.5f, 0.5f, 1f );
+                        asteroidLevel++;
+                        growthRateActual *= growthRateDecay;
+                        break;
+                    case 3:
+                        asteroidScale = Vector3.one;
+                        break;
+                    default:
+                        Debug.LogWarning( "Asteroid level out of bounds" );
+                        break;
+                }
             }
 
+            heldAsteroid.transform.localScale = asteroidScale;
         }
+        else
+        {
+            asteroidScale = asteroidScale = startingAsteroidScale;
+            asteroidLevel = 0;
+            growthRateActual = growthRateDefault;
+            heldAsteroid = null;
+        }
+
     }
 
     void OnDrawGizmos()
     {
-        Ray r = new Ray( mainCamera.transform.position, Input.mousePosition );
-        Gizmos.DrawRay( r );
+
     }
 }
