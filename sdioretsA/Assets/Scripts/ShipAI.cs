84b709afa5ee03a14e9fe4705c70665f8b1ba3ba
Home <androulakakis.2@wright.edu>
2022-02-02 22:59:38 -0500

    Started messing around with the 2d physics.
    Made player 2. It will hit the asteroids and moved forward with force by pressing arrow up. Still need to figure out how to turn it

--- sdioretsA/Assets/Scripts/ShipAI.cs
+++ sdioretsA/Assets/Scripts/ShipAI.cs
@@ -14,21 +14,29 @@ public class ShipAI : MonoBehaviour
     [SerializeField]
     private BulletManager bulletManager;
 
+    [SerializeField]
+    private KeyCode forward;
+
+     [SerializeField]
+    private KeyCode left;
+
+     [SerializeField]
+    private KeyCode right;
+
+    [SerializeField]
+    private Rigidbody2D playerBody;
+
     // Various movement related variables
     // Translational acceleration
     [SerializeField]
-    private float moveRate;
+    private float forwardThrust;
     // Rotational acceleration
     [SerializeField]
-    private float rotRate;
+    private float turnThrust;
     [SerializeField]
     private float maxSpeed;
     [SerializeField]
     private float maxRotation;
-    // This one makes it so if you're flying in one direction and aiming in another, pressing forward doesn't instantly change your direction
-    // to 100% your new forward and instead slowly corrects in that direction.  The greater this value, the slower the course correction is
-    [SerializeField]
-    private int directionalSluggishness;
 
     // All the current movement values
     [SerializeField]
@@ -36,35 +44,61 @@ public class ShipAI : MonoBehaviour
     [SerializeField]
     private float currentSpeed;
     [SerializeField]
-    private Vector3 currentDirection;
+    private Vector2 currentDirection;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( !shipManager.playerControlled )
-        {
+        
 
-            // Set Rotation
+            // Apply Thrust
+            if( Input.GetKey( forward ) )
+            {
+                // Mathf.Clamp will take the value of the first argument and clamp it within the minimum and maximum values set by the 2nd and 3rd params respectively
+                playerBody.AddForce(transform.up*forwardThrust, ForceMode2D.Force);
+                Debug.Log( transform.up*forwardThrust);
+            }
+            // Turn Left
+            if( Input.GetKey( left ) )
+            {
+                // Mathf.Clamp will take the value of the first argument and clamp it within the minimum and maximum values set by the 2nd and 3rd params respectively
+                playerBody.AddTorque((-5 * Mathf.Deg2Rad) * playerBody.inertia);
+            }
+            // Turn Right
+            if( Input.GetKey( left ) )
+            {
+                // Mathf.Clamp will take the value of the first argument and clamp it within the minimum and maximum values set by the 2nd and 3rd params respectively
+                playerBody.AddTorque((5 * Mathf.Deg2Rad) * playerBody.inertia);
+            }
 
-            // Set Speed
 
-        }
+        
     }
 
     void LateUpdate()
     {
-        if( !shipManager.playerControlled )
+        
+    }
+
+    // When the ship collider + rigidbody collides with another collider that has 'IsTrigger = true'
+    void OnTriggerEnter2D( Collider2D col )
+    {
+        // Make sure the game object we collided with has a tag of "Border" meaning it's reached far enough off screen that there's a clone already
+        // running around in that the player is using as the main ship.  Time to destroy this oen to prevent weird stuff from happening
+        if( col.gameObject.tag == "Border" )
         {
-            // Apply movement changes
-            transform.position += currentDirection.normalized * currentSpeed * Time.deltaTime;
-            transform.Rotate( Vector3.forward * currentRotation * Time.deltaTime, Space.World );
+            // Reset edge logic so we can now loop again
+            edgeLogic.ResetEdgeUse();
+
+            // Bye bye ship
+            Destroy( gameObject );
         }
     }
 }
